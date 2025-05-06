598740cc39c8901bf2089fe1e9ddd024c199b468
Isabella <isabella.tds1.senai@gmail.com>
2025-05-06 08:08:40 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🛍️  Servidor rodando em http://localhost:${PORT}`);
+});
