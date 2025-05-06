459737ec79e479ee4144088a9370af1e3027f531
Isabella <isabella.tds1.senai@gmail.com>
2025-05-06 11:07:41 -0300

    feat: create API KEY

--- src/routes/produtoRoutes.js
+++ src/routes/produtoRoutes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const produtoController = require("../controllers/produtoController");
-const upload = require("../config/upload")
+const upload = require("../config/upload");
+const apiKeyMiddleware = require("../config/apiKey"); 
 
+router.use(apiKeyMiddleware);
 router.get("/produto", produtoController.getAllProdutos);router.get("/produto/:id", produtoController.getProdutoById);
 router.post("/produto", upload.single("photo"),produtoController.createProduto);
 router.put("/produto/:id", produtoController.updateProduto);
--- src/routes/vendaRoutes.js
+++ src/routes/vendaRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const vendaController = require("../controllers/vendaController");
+const apiKeyMiddleware = require("../config/apiKey"); 
 
+router.use(apiKeyMiddleware);
 router.get("/venda", vendaController.getAllVendas);
 router.get("/venda/:id", vendaController.getVenda);
 router.post("/venda", vendaController.createVenda);
